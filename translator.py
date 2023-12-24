f5545b297d37701cd3b02be5483175bc3fd7a306
fevin1414 <fevinbiju1414@gmail.com>
2023-12-24 12:16:02 -0600

    added a stop command

--- translator.py
+++ translator.py
@@ -2,33 +2,64 @@ import googletrans
 from googletrans import Translator
 import speech_recognition as sr
 from gtts import gTTS
+import pygame
+import io
+import tempfile
 import os
+import time
 
-# print(googletrans.LANGUAGES)
 translator = Translator()
 
 # To capture audio
 mic = sr.Microphone()
 rec = sr.Recognizer()
 
-with mic as source:
-    inp_lang = 'hi'
-    out_lang = 'en'
+while True:
+    with mic as source:
+        inp_lang = 'hi'
+        out_lang = 'en'
 
-    print("Please speak now...")
+        print("Please speak now...")
 
-    rec.adjust_for_ambient_noise(source, duration=0.2)
+        rec.adjust_for_ambient_noise(source, duration=0.2)
 
-    try:
         audio = rec.listen(source)
-        rec_aud = rec.recognize_google(audio)
-        print(f"Recognized text: {rec_aud}")
 
-        # Translate the recognized text
-        translation = translator.translate(rec_aud, src=inp_lang, dest=out_lang)
-        print(f"Translation: {translation.text}")
+        try:
+            rec_aud = rec.recognize_google(audio)
+            print("Here is the audio input: " + rec_aud)
 
-    except sr.UnknownValueError:
-        print("Speech Recognition could not understand audio")
-    except sr.RequestError as e:
-        print(f"Could not request results from Google Speech Recognition service; {e}")
+
+            if rec_aud.lower() == "stop":
+                print("Stopping the program.")
+                break
+
+            to_translate = translator.translate(rec_aud, src=inp_lang, dest=out_lang)
+            translated_text = to_translate.text
+            print("The translated text is: ", translated_text)
+
+            with tempfile.NamedTemporaryFile(delete=False) as temp_audio_file:
+                speak = gTTS(text=translated_text, lang=out_lang, slow=False)
+                speak.save(temp_audio_file.name)
+
+            pygame.mixer.init()
+
+            pygame.mixer.music.load(temp_audio_file.name)
+
+            pygame.mixer.music.play()
+
+            while pygame.mixer.music.get_busy():
+                pygame.time.Clock().tick(10)
+
+            time.sleep(1)
+
+            try:
+                os.remove(temp_audio_file.name)
+            except PermissionError:
+                print("PermissionError: The file is still in use and cannot be deleted immediately.")
+                print("You might want to handle the deletion at a later point in your program.")
+
+        except sr.UnknownValueError:
+            print("Speech Recognition could not understand audio")
+        except sr.RequestError as e:
+            print(f"Could not request results from Google Speech Recognition service; {e}")
